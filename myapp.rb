852137f21d793472fe9ae99a915aa21c33d4e7ac
Saad Khan <Saadkx@gmail.com>
2022-01-05 15:00:26 +0000

    added cat with borders

--- myapp.rb
+++ myapp.rb
@@ -8,3 +8,13 @@ end
 get '/secret' do 
   'This is my secret stash'
 end
+
+get '/cat' do 
+  "<div style='border: 3px dashed red'>
+   <img src='https://i.imgur.com/jFaSxym.png'>
+   </div>"
+end
+
+
+# <div> <img src="pic_trulli.jpg" alt="Trulli" width="500" height="333"> </div>
+
